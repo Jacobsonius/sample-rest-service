37e3f8a846727fa56ae35d9485383e24e017e490
Jacobsonius <Jacobsonius@users.noreply.github.com>
2025-04-28 19:56:26 +0000

    QA Assistance: JUnit tests updated

--- src/test/java/com/sampleservice/demo/DemoApplicationTests.java
+++ src/test/java/com/sampleservice/demo/DemoApplicationTests.java
@@ -2,15 +2,30 @@ package com.sampleservice.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext applicationContext;
 
+    @Test
+    public void contextLoads() {
+        // Ensure the application context is not null
+        assertNotNull("Application Context should not be null", applicationContext);
+    }
+
+    @Test
+    public void testKeyBeansArePresent() {
+        // Verify that specific beans are present in the application context
+        assertNotNull("WebApplicationContext bean should be available", applicationContext.getBean("webApplicationContext"));
+        assertNotNull("dataSource bean should be available", applicationContext.getBean("dataSource"));
+    }
 }
